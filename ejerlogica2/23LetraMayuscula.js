234ab5ec0f778b557c97bfc5ee7f019590b1e321
Programacion <programacion@iMac-de-Programacion.local>
2022-07-06 07:43:50 -0500

    hasta 30

--- ejerlogica2/23LetraMayuscula.js
+++ ejerlogica2/23LetraMayuscula.js
@@ -1,20 +1,16 @@
 // Dada una cadena de texto cambiar la primera letra de cada palabra a mayuscula
 
 
-const cambiarAMayuscula = ( cadena) => {
-
+const cambiarAMayuscula = (cadena) => {
     let resultado = ''
-     
     for (const letra in cadena) {
-        
-        if(cadena[letra -1] === ' ' || parseInt(letra) === 0){
+        if (cadena[letra - 1] === ' ' || parseInt(letra) === 0) {
             resultado += cadena[letra].toUpperCase();
         }
-        else{
-            resultado +=cadena[letra]
+        else {
+            resultado += cadena[letra]
         }
     }
-
     return resultado;
 }
 
@@ -23,16 +19,18 @@ console.log(cambiarAMayuscula('hola como estan'));
 
 //-----------------------------------------
 
-const cambiarAMayusculaB = ( cadena) => {
-
+const cambiarAMayusculaB = (cadena) => {
     let palabras = [];
-     
-   for (const palabra of cadena.split(' ')) {
-
-    palabras.push(palabra[0].toUpperCase() + palabra.slice(1))
-   }
-
+    for (const palabra of cadena.split(' ')) {
+        palabras.push(palabra[0].toUpperCase() + palabra.slice(1))
+    }
     return palabras.join(' ');
 }
-
 console.log(cambiarAMayusculaB('hola como estan'));
+
+let hora = 30;
+numHorasdia = 3;
+
+const dia = numHorasdia * hora
+let semana = dia * 5;
+console.log(semana);
