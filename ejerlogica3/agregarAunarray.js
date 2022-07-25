aacc224b73007ab37c0a5e5bb7f22ca14de0f3f0
Carlos <alfredomontoyagit@gmail.com>
2022-07-25 08:20:24 -0500

    Ejercicios de array

---
+++ ejerlogica3/agregarAunarray.js
@@ -0,0 +1,24 @@
+// --- Agregar elementos a un array -----
+
+
+const array1 = ["Carlos", "Alfredo"];
+
+// ------------ Agregar al final ----------------
+
+array1.push("Montoya");
+
+
+// ------------ Agregar al inicio ----------------
+
+array1.unshift("Goez");
+
+array1.splice (1,0, "develop");
+
+console.log(array1);
+// ------------ Concatenar matriz ----------------
+
+const esposa = ["Andrea", "Montoya"];
+
+const newArray = array1.concat(esposa);
+
+console.log(newArray);
